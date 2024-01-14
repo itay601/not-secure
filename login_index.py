ef5081eb30c6fccb9d8745c76a9f782504f3fd0a
itay601 <itay601@LAPTOP-UI69H6FF>
2024-01-14 20:12:29 +0200

    "adding request flask to get input from html files"

--- login_index.py
+++ login_index.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print('Username:', username)
+        print('Password:', password)
     return render_template('login.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True
