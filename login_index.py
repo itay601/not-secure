0fcaeba5379a918cdf127ce8a586da4b2cd828bf
itay601 <itay601@LAPTOP-UI69H6FF>
2024-01-16 10:12:01 +0200

    all works ! now input validation !

--- login_index.py
+++ login_index.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,12 +8,23 @@ def index():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        #if username==s:
-         #   print(1)
-        print('Username:', username)
+    
+        print('Username1:', username)
         print('Password:', password)
+        #check and works!!! 
+        if username == "s":
+            return render_template('system_screen.html')
+        #now we need here to do input validation 
+        # for login file     
+
+
+
+
     return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
   
+
+
